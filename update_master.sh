d568e4b743f5088bc7ae9f48841f1269f4e3a633
Rahul Mehta <mehta.rahul95@gmail.com>
2017-07-10 23:02:43 -0700

    Streamline update_master script

--- update_master.sh
+++ update_master.sh
@@ -1,22 +1,29 @@
 #! /bin/bash
-git remote add upstream \ 
-    git@github.com:rahulsmehta/react-redux-typescript-boilerplate.git >/dev/null \
-        2>/dev/null
-if [ $? -ne 0 ]; then
-    REMOTE=`git remote -v | grep upstream | grep fetch`
-    echo "Using ${REMOTE}"
-fi
+UPSTREAM=git@github.com:rokoroku/react-redux-typescript-boilerplate.git
+run_fetch() {
+    git fetch upstream; 
+    if [ $? -ne 0 ]; then
+        exit 1;
+    fi
+    git rebase upstream/master; 
+    if [ $? -ne 0 ]; then
+        exit 1;
+    fi
+}
+git remote add upstream  $UPSTREAM > /dev/null 2> /dev/null ;
 
-git fetch upstream
-if [ $? -ne 0 ]; then
-    echo "Fetch failed..exiting.."
-    exit 1
-fi
+REMOTE=`git remote -v | grep upstream | grep fetch`
+echo "Using ${REMOTE}"
+
 while true; do
-    read -p "Start update from upstream/master? [Yy|Nn]: " yn
+    read -p "Start update from upstream/master? [y|n]: " yn
     case $yn in
-        [Yy]* ) git rebase upstream/master; break;;
+        [Yy]* ) 
+            run_fetch;
+            break;;
         [Nn]* ) exit;;
         * ) ;;
     esac
 done
+exit 0
+
